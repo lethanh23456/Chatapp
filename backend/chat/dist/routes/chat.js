6dbbca691c0d4a32c28f375830f4e0fb0302a5fd
lethanh23456 <thanhld2006.ptit@gmail.com>
2026-01-18 13:53:29 +0700

    up

--- backend/chat/dist/routes/chat.js
+++ backend/chat/dist/routes/chat.js
@@ -1,2 +1,7 @@
-export {};
+import express from 'express';
+import { isAuth } from '../middleware/isAuth.js';
+import { createNewChat } from '../controllers/chat.js';
+const router = express.Router();
+router.post("/chat/new", isAuth, createNewChat);
+export default router;
 //# sourceMappingURL=chat.js.map
