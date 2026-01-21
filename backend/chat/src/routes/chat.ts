e89aa58dc3f061c45d655f56b08c9b8e3cf2aa63
lethanh23456 <thanhld2006.ptit@gmail.com>
2026-01-21 23:23:01 +0700

    fe web

--- backend/chat/src/routes/chat.ts
+++ backend/chat/src/routes/chat.ts
@@ -1,6 +1,6 @@
 import express from 'express';
 import { isAuth } from '../middleware/isAuth.js';
-import { createNewChat, getAllChats, sendMessage } from '../controllers/chat.js';
+import { createNewChat, getAllChats, getMessagesByChat, sendMessage } from '../controllers/chat.js';
 import { upload } from '../middleware/multer.js';
 
 const router = express.Router();
@@ -8,5 +8,6 @@ const router = express.Router();
 router.post("/chat/new",isAuth, createNewChat);
 router.get("/chat/all",isAuth, getAllChats );
 router.post("/message", isAuth, upload.single('image'), sendMessage);
+router.get("/message/:chatId", isAuth , getMessagesByChat);
 
 export default router;
